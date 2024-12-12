75f6cee4f29d15f012cb2b78afbaf9964b2d58cb
GrownUprince <jake.lee4006@gmail.com>
2024-12-12 14:56:44 +0900

    fix typescript-eslint error/no-unused-vars

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -55,6 +55,7 @@ export async function createInvoice(prevState: State, formData: FormData) {
       VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
     `;
   } catch (error) {
+    console.log(error);
     return {
       message: 'Database Error: Failed to Create Invoice.',
     };
@@ -92,6 +93,7 @@ export async function updateInvoice(
           WHERE id = ${id}
         `;
   } catch (error) {
+    console.log(error);
     return { message: 'Database Error: Failed to Update Invoice.' };
   }
 
@@ -105,6 +107,7 @@ export async function deleteInvoice(id: string) {
     revalidatePath('/dashboard/invoices');
     return { message: 'Deleted Invoice.' };
   } catch (error) {
+    console.log(error);
     return { message: 'Database Error: Failed to Delete Invoice.' };
   }
 }
